84e32e89fe83f304fce5e2315ba9c5285181a1e3
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-09-04 20:15:56 -0300

    Update cidade.py

--- Aulas/Aula_string/lições_string/cidade.py
+++ Aulas/Aula_string/lições_string/cidade.py
@@ -4,3 +4,7 @@ print(v1)
 
 #.startswith() verifica se a string começa com o valor especificado
 #.endswith() verifica se a string termina com o valor especificado
+
+cidade1=str(input("Digite o nome da sua cidade-->"))
+v2=cidade1.split
+print("santo" in (cidade1 [0]))
