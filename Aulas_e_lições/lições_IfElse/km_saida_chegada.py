de06524765af1d3125c46c5b50dca5d51b31cb5a
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-09-09 19:41:28 -0300

    .

--- Aulas_e_lições/lições_IfElse/km_saida_chegada.py
+++ Aulas_e_lições/lições_IfElse/km_saida_chegada.py
@@ -1,2 +1,15 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
+
+ks=float(input("Qual o KM de saída do seu carro?"))
+kc=float(input("Qual o KM de chegada?"))
+hs=float(input("Quando você saiu?"))
+hc=float(input("Quando você chegou?"))
+
+media=ks-kc/hs-hc
+
+if media<80:
+print("multa de 5 reais para você amigo")
+
+else:
+print("continue na linha")
