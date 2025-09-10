75ec4721660e0caf10e2f9a9fd87b313b596823d
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-09-09 21:13:35 -0300

    .

--- Aulas_e_lições/lições_IfElse/ano_bisexto.py
+++ Aulas_e_lições/lições_IfElse/ano_bisexto.py
@@ -1,5 +1,5 @@
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls')
 
 ano=int(input("Digite um ano-->"))
 if (ano%4==0 and ano%100!=0) or (ano%400==0): #se o ano for divisível por 4 e não for divisível por 100, ou se for divisível por 400
