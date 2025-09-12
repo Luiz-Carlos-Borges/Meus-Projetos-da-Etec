15ecb6db4e9d93a811ba6557a4d18cbbddb467a9
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-09-11 21:40:08 -0300

    .

--- Aulas_e_lições/Lições_do_dia_09/jokenpo.py
+++ Aulas_e_lições/Lições_do_dia_09/jokenpo.py
@@ -7,3 +7,24 @@ itens = ["pedra", "papel", "tesoura"]
 computador = random.choice (itens)
 jogador = int(input("Escolha sua opção: [0] pedra, [1] papel, [2] tesoura: "))
 print("O computador escolheu", format(computador))
+if jogador == 0:
+    if computador == "pedra":
+        print("Empate")
+    elif computador == "papel":
+        print("Computador ganhou")
+    elif computador == "tesoura":
+        print("Jogador ganhou")
+elif jogador == 1:
+    if computador == "pedra":
+        print("Jogador ganhou")
+    elif computador == "papel":
+        print("Empate")
+    elif computador == "tesoura":
+        print("Computador ganhou")
+elif jogador == 2:
+    if computador == "pedra":
+        print("Computador ganhou")
+    elif computador == "papel":
+        print("Jogador ganhou")
+    elif computador == "tesoura":
+        print("Empate")
