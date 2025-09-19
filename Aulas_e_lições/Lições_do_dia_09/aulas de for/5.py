4ee135c3737dd8d4505f70caa342233d5b80414b
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-09-18 21:58:32 -0300

    .

--- Aulas_e_lições/Lições_do_dia_09/aulas de for/5.py
+++ Aulas_e_lições/Lições_do_dia_09/aulas de for/5.py
@@ -5,4 +5,4 @@ soma = 0
 for c in range(1,4): #pede para digitar 4 vezes
     n=int(input("Digite um número ai chefe-->"))
     soma= n+c# vai somar o número digitado mais o resultado que foi somado
-    print("a somatória foi", soma) 
+    print("a somatória foi", soma)
