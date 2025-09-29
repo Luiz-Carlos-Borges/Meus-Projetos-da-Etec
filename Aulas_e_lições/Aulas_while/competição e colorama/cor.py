ba71128029c38957d1b6c59d418f50ddde647528
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-09-29 20:06:38 -0300

    .

--- Aulas_e_lições/Aulas_while/competição e colorama/cor.py
+++ Aulas_e_lições/Aulas_while/competição e colorama/cor.py
@@ -4,5 +4,3 @@ colorama.init()
 print(colorama.Fore.RED +'texto') #troca a letra dos textos
 print(colorama.Back.GREEN +'texto') #texto fundo
 #print(colorama.Fore.RESET) #Retorna a cor padrão
-
-#print('carlinhos engolindo mijo'*50)
