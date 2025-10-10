44e3811a3b14fbc7dd4c80555d69106319540e84
Luiz-Carlos-Borges <luizcbdamico1@gmail.com>
2025-10-09 22:11:28 -0300

    .

--- Aulas_e_lições/tupla e lista/Ex1.py
+++ Aulas_e_lições/tupla e lista/Ex1.py
@@ -1,11 +1,20 @@
 import os
 os.system('cls')
 
-lista = list(range(0,10))
+numeros = [] # lista para armazenar todos os números
+pares = [] # lista para armazenar os números pares
+impares = [] # lista para armazenar os números ímpares
 
-lista2 = lista[:]
+while True: # loop infinito, só para quando o usuário digitar -1
+    n = int(input("Digite um número (ou -1 para parar): ")) # lê um número do usuário
+    if n == -1: # se o número for -1, sai do loop
+        break  
+    numeros.append(n) # adiciona o número à lista completa
+    if n % 2 == 0: # se o número for par
+        pares.append(n) # adiciona à lista de pares
+    else:   # se o número for ímpar
+        impares.append(n) # adiciona à lista de ímpares
 
-lista3 =  lista[:]
-
-print(lista)
-print(lista,:2)
+print("Lista completa:", numeros) # imprime a lista completa
+print("Lista de pares:", pares) # imprime a lista de pares
+print("Lista de ímpares:", impares) # imprime a lista de ímpares  
