8a9ee323a561179634cf19bd8002ec03bb319f7a
Luiz Carlos Borges D'Amico <luizcbdamico1@gmail.com>
2025-10-16 21:32:09 -0300

    .

--- trio.py
+++ trio.py
@@ -1,60 +1,25 @@
 import os
 os.system('cls')
 
-numeros=[]
-pares=[]
-impares=[]
+numeros = []
+pares = []
+impares = []
 
-
-print('seja Bem vindo!')
+print('Seja Bem-vindo!')
 print('\nVamos digitar seu cadastro')
 
-while True:
+while len(numeros) < 7:  # O loop continuará enquanto houver menos de 7 números cadastrados
+    try:
+        n = int(input("Digite cada número do seu cadastro --> "))
+        numeros.append(n)
+        if n % 2 == 0:
+            pares.append(n)
+        else:
+            impares.append(n)
+    except ValueError:
+        print("Erro: Entrada inválida. Digite um número inteiro.")
 
-
-
-
-
-
-
-
-
-    e = (input("Digite seu cadastro--> "))
-    if len(e) == 7:
-        print("Bem-vindo")
-    elif len(e) < 7:
-        print("Entrada inválida:", e)
-    else:
-        print('Entrada inválida')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        if e==e:
-            numeros.append(e)
-        if e % 2 == 0:
-            pares.append(e)
-        else: 
-            impares.append(e)
-
-        print("matrícula completa:", numeros)
-        print("Números pares:", pares)
-        print("Números ímpares:", impares) 
+print("Cadastro completo!")
+print("Lista completa:", numeros)
+print("Lista de pares:", pares)
+print("Lista de ímpares:", impares)
