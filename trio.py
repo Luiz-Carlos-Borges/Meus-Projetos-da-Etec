61bd78de4d3a3bef4b59fbdf41a7fb3d8fc23308
Luiz Carlos Borges D'Amico <luizcbdamico1@gmail.com>
2025-10-16 21:51:13 -0300

    .

--- trio.py
+++ trio.py
@@ -9,7 +9,7 @@ print('Seja Bem-vindo!')
 print('\nVamos digitar seu cadastro')
 
 while True:
-    # Entrada dos números
+ 
     p = int(input('Digite o primeiro número do seu cadastro: '))
     s = int(input('Digite o segundo número do seu cadastro: '))
     t = int(input('Digite o terceiro número do seu cadastro: '))
@@ -18,20 +18,18 @@ while True:
     sex = int(input('Digite o sexto número do seu cadastro: '))
     sete = int(input('Digite o sétimo número do seu cadastro: '))
 
-    # Adiciona os números à lista principal
-    numeros.extend([p, s, t, q, qi, sex, sete])
+  
+    numeros.extend([p, s, t, q, qi, sex, sete]) #extend adiciona vários elementos de uma vez enquanto o append acrescenta um elemento por vez
 
-    # Classifica os números em pares e ímpares
+  
     for n in [p, s, t, q, qi, sex, sete]:
         if n % 2 == 0:
             pares.append(n)
         else:
             impares.append(n)
 
-    # Exibe os resultados
     print("\nMatrícula completa:", numeros)
     print("Números pares:", pares)
     print("Números ímpares:", impares)
 
-    # Encerra o loop após o cadastro
     break
